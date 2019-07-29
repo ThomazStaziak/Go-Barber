1470fe35b0e2e7d6c7c72f2c356dfa5bbde8f0f0
ThomazStaziak <thostaziak@gmail.com>
2019-07-29 17:13:34 -0300

    creating appointment routes

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import multer from 'multer';
+
 import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -19,6 +22,9 @@ routes.use(authMiddleware);
 routes.put('/user', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
